7c9650d8e23808b4093d8c544a69338d34e63f5e
ivanovskipetar <petar.ivanovski@codeit.mk>
2024-06-12 17:14:47 +0200

    displayed image for tour & added url to list of supported urls in next.config

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+const url = "https://www.course-api.com/images/tours/tour-1.jpeg";
 function Page({ params }: { params: { id: string } }) {
   return (
     <div>
       <h1 className="text-4xl">ID: {params.id}</h1>
+      <section className="flex gap-x-4 mt-4"></section>
+      {/* remote image*/}
+      <div>
+        <Image
+          src={url}
+          alt="tour"
+          width={192}
+          height={192}
+          className="w-48 h-48 object-cover rounded"
+        />
+        <h2>Remote image</h2>
+      </div>
     </div>
   );
 }
