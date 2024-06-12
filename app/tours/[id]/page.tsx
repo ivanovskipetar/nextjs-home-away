0e4f25b7d2388091f2e00436533d83ccc10ff6c8
ivanovskipetar <petar.ivanovski@codeit.mk>
2024-06-12 16:48:43 +0200

    added dynamic page for specific tour display

---
+++ app/tours/[id]/page.tsx
@@ -0,0 +1,9 @@
+function Page({ params }: { params: { id: string } }) {
+  return (
+    <div>
+      <h1 className="text-4xl">ID: {params.id}</h1>
+    </div>
+  );
+}
+
+export default Page;
--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 const url = "https://www.course-api.com/react-tours-project";
 
@@ -25,7 +26,15 @@ async function ToursPage() {
     <section>
       <h1 className="text-3xl mb-4">Tours</h1>
       {data.map((tour) => {
-        return <h2 key={tour.id}>{tour.name}</h2>;
+        return (
+          <Link
+            key={tour.id}
+            href={`/tours/${tour.id}`}
+            className="hover:text-blue-500"
+          >
+            <h2>{tour.name}</h2>
+          </Link>
+        );
       })}
     </section>
   );
