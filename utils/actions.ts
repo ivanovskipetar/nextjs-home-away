6f139ce588cd58b5c738368c4d0b5c65ba3c80df
ivanovskipetar <petar.ivanovski@codeit.mk>
2024-06-13 18:17:46 +0200

    moved createUser as a separate action & added json data which serves as database. Displayed users after each successful addition.

---
+++ utils/actions.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import { readFile, writeFile } from "fs/promises";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+type User = {
+  id: string;
+  firstName: string;
+  lastName: string;
+};
+
+export const createUser = async (formData: FormData) => {
+  "use server";
+
+  const firstName = formData.get("firstName") as string;
+  const lastName = formData.get("lastName") as string;
+
+  const newUser: User = { firstName, lastName, id: Date.now().toString() };
+
+  try {
+    await saveUser(newUser);
+  } catch (error) {
+    console.log(error);
+  }
+
+  //revalidatePath('/actions');
+  redirect("/");
+};
+
+export const fetchUsers = async (): Promise<User[]> => {
+  const result = await readFile("users.json", { encoding: "utf8" });
+  const users = result ? JSON.parse(result) : [];
+
+  return users;
+};
+
+const saveUser = async (user: User) => {
+  const users = await fetchUsers();
+  users.push(user);
+  await writeFile("users.json", JSON.stringify(users));
+};
